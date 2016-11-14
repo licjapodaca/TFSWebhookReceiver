39700c39bbc80a99c0bb080caf060a2e62c8a55b
M.C.S.D. Jorge Apodaca <japodaca@bts.com.mx>
2016-11-14 03:25:57 -0800

    Many classes added

--- TFSWebhookReceiver/Controllers/ReceiverController.cs
+++ TFSWebhookReceiver/Controllers/ReceiverController.cs
@@ -3,12 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Configuration;
 using System.Web.Http;
+using TFSWebhookReceiver.Models;
 
 namespace TFSWebhookReceiver.Controllers
 {
-    public class ReceiverController : ApiController
-    {
+	public class ReceiverController : ApiController
+	{
+		static HttpClient client = new HttpClient();
 
-    }
+		[HttpPost]
+		public async Task<IHttpActionResult> BuildCompleteEvent(BuildCompleteEvent parameters)
+		{
+			var card = new ConnectorCard();
+			string urlWebhook = WebConfigurationManager.AppSettings["build.complete.webhook"].ToString();
+
+
+			card.text = "Hello World!";
+			card.summary = "Este es el summary";
+			card.title = "Y este es el titulo";
+			card.themeColor = "#737373";
+			card.sections = new List<Section>()
+			{
+				new Section()
+				{
+					activityTitle = "Titulo Seccion",
+					activitySubtitle = "Subtitulo Seccion",
+					activityText = "Texto de la Eccion",
+					activityImage = parameters.resource.requestedBy.imageUrl,
+					facts = new List<Fact>()
+					{
+						new Fact() { name = "Build", value = parameters.resource.buildNumber },
+						new Fact() { name = "URL", value = "[Prueba](http://svr-tfs:8080/tfs)" }
+					}
+				}
+			};
+
+
+			HttpResponseMessage response = await client.PostAsJsonAsync(urlWebhook, card);
+			response.EnsureSuccessStatusCode();
+
+			return Ok(new { success = true });
+		}
+	}
 }
---
+++ TFSWebhookReceiver/Models/ConnectorCard.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TFSWebhookReceiver.Models
+{
+	public class ConnectorCard
+	{
+		public string summary { get; set; }
+		public string text { get; set; }
+		public string title { get; set; }
+		public string themeColor { get; set; }
+
+		public List<Section> sections { get; set; }
+		public List<PotentialAction> potentialAction { get; set; }
+	}
+
+	public class Section
+	{
+		public string title { get; set; }
+		public string activityTitle { get; set; }
+		public string activitySubtitle { get; set; }
+		public string activityImage { get; set; }
+		public string activityText { get; set; }
+		public string text { get; set; }
+		public bool markdown { get; set; }
+
+		public List<Fact> facts { get; set; }
+		public List<Image> images { get; set; }
+		public List<PotentialAction> potentialAction { get; set; }
+	}
+
+	public class Fact
+	{
+		public string name { get; set; }
+		public string value { get; set; }
+	}
+
+	public class Image
+	{
+		public string title { get; set; }
+		public string image { get; set; }
+	}
+
+	public class PotentialAction
+	{
+		public string name { get; set; }
+		public string target { get; set; }
+		public string @context { get; set; }
+		public string @type { get; set; }
+	}
+}
