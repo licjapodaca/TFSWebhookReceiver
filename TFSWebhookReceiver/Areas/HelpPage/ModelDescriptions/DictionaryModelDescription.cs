f983337bde86fde8b3f66a3488f7820b129a0361
M.C.S.D. Jorge Apodaca <japodaca@bts.com.mx>
2016-11-13 20:09:40 -0800

    Initial prooject

---
+++ TFSWebhookReceiver/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace TFSWebhookReceiver.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ TFSWebhookReceiver/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace TFSWebhookReceiver.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
