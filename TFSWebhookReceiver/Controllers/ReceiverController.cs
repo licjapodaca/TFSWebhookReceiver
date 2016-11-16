da2ede3cb690726b4b35bba9bc105a6b0e5ee6b7
M.C.S.D. Jorge Apodaca <japodaca@bts.com.mx>
2016-11-16 00:41:50 -0800

    Final changes to Build.Complete event method

--- TFSWebhookReceiver/Controllers/ReceiverController.cs
+++ TFSWebhookReceiver/Controllers/ReceiverController.cs
@@ -21,6 +21,22 @@ namespace TFSWebhookReceiver.Controllers
 		{
 			try
 			{
+				parameters.resource.queueTime = parameters.resource.queueTime.Kind == DateTimeKind.Unspecified ? DateTime.Now : parameters.resource.queueTime;
+				parameters.resource.buildNumberRevision = parameters.resource.buildNumberRevision == 0 ? 1 : parameters.resource.buildNumberRevision;
+
+				parameters.resourceVersion = parameters.resourceVersion == null ? "Test" : parameters.resourceVersion;
+				parameters.resource.result = parameters.resource.result == null ? "Test" : parameters.resource.result;
+				parameters.resource.sourceBranch = parameters.resource.sourceBranch == null ? "Test" : parameters.resource.sourceBranch;
+				parameters.resource.sourceVersion = parameters.resource.sourceVersion == null ? "Test" : parameters.resource.sourceVersion;
+				parameters.resource.priority = parameters.resource.priority == null ? "Test" : parameters.resource.priority;
+				parameters.resource.requestedFor = parameters.resource.requestedFor == null ? new TfsUser() { id = Guid.NewGuid(), displayName = "Test", imageUrl = "http://Test", uniqueName = "Test", url = "http://Test" } : parameters.resource.requestedFor;
+				parameters.resource.requestedBy = parameters.resource.requestedBy == null ? new TfsUser() { id = Guid.NewGuid(), displayName = "Test", imageUrl = "http://Test", uniqueName = "Test", url = "http://Test" } : parameters.resource.requestedBy;
+
+				if (!ModelState.IsValid)
+				{
+					return BadRequest(ModelState);
+				}
+
 				var card = new ConnectorCard();
 				string urlWebhook = WebConfigurationManager.AppSettings["build.complete.webhook"].ToString(),
 					logoUrl = WebConfigurationManager.AppSettings["urlLogoEncoded"].ToString();
@@ -28,20 +44,20 @@ namespace TFSWebhookReceiver.Controllers
 				card.text = "# Build Process Completed";
 				card.themeColor = "#737373";
 
-				card.sections = new List<Section>()
-				{
+				card.sections = new List<Section>();
+				card.sections.Add(
 					new Section()
 					{
 						activityTitle = "Build executed by TFS Continues Integration",
 						activitySubtitle = "Details notification",
-						activityText = String.Format("**Requested by:** {0}<br/>**Domain user:** {1}", String.IsNullOrEmpty(parameters.resource.requestedBy.displayName) ? "Test" : parameters.resource.requestedBy.displayName, String.IsNullOrEmpty(parameters.resource.requestedBy.uniqueName) ? "Test" : parameters.resource.requestedBy.uniqueName),
+						activityText = String.Format("**Requested for:** {0}<br/>**Domain user:** {1}", String.IsNullOrEmpty(parameters.resource.requestedFor.displayName) ? "Test" : parameters.resource.requestedFor.displayName, String.IsNullOrEmpty(parameters.resource.requestedFor.uniqueName) ? "Test" : parameters.resource.requestedFor.uniqueName),
 						activityImage = HttpUtility.UrlDecode(logoUrl),
 						markdown = true
-					},
-					new Section()
-					{
-						title = "**Information Details**",
-						facts = new List<Fact>()
+					});
+				var section = new Section()
+				{
+					title = "**Information Details**",
+					facts = new List<Fact>()
 						{
 							new Fact() { name = "Build: ", value = String.Format("{0}", parameters.resource.buildNumber) },
 							new Fact() { name = "URL: ", value = String.Format("**{0}**", parameters.message.markdown) },
@@ -52,10 +68,15 @@ namespace TFSWebhookReceiver.Controllers
 							new Fact() { name = "Build created on: ", value = String.Format("{0}", parameters.createdDate.ToString("dd/MM/yyyy h:mm:ss tt")) },
 							new Fact() { name = "Result: ", value = String.Format("##### <span style='color:{0};'>**{1}**</span>", String.IsNullOrEmpty(parameters.resource.result) ? "TEST" : parameters.resource.result.ToUpper() == "FAILED" ? "red" : "darkblue", String.IsNullOrEmpty(parameters.resource.result) ? "TEST" : parameters.resource.result.ToUpper()) }
 						},
-						markdown = true
-					}
+					markdown = true
 				};
 
+				if (parameters.resource.result.ToUpper() == "FAILED")
+				{
+					section.facts.Add(new Fact() { name = "Error message: ", value = parameters.detailedMessage.markdown });
+				}
+
+				card.sections.Add(section);
 
 				HttpResponseMessage response = await client.PostAsJsonAsync(urlWebhook, card);
 				response.EnsureSuccessStatusCode();
--- TFSWebhookReceiver/Models/BuildCompleteEvent.cs
+++ TFSWebhookReceiver/Models/BuildCompleteEvent.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,59 +8,103 @@ namespace TFSWebhookReceiver.Models
 {
 	public class BuildCompleteEvent
 	{
+		
 		public Guid subscriptionId { get; set; }
+		
 		public int notificationId { get; set; }
+		
 		public Guid id { get; set; }
+		
 		public string eventType { get; set; }
+		
 		public string publisherId { get; set; }
+		
 		public string resourceVersion { get; set; }
+		
 		public DateTime createdDate { get; set; }
 
+
+		
 		public Message message { get; set; }
+		
 		public DetailedMessage detailedMessage { get; set; }
+		
 		public Resource resource { get; set; }
 	}
 
 	public class Message
 	{
+
+		
 		public string text { get; set; }
+
+		
 		public string html { get; set; }
+
+		
 		public string markdown { get; set; }
 	}
 
 	public class DetailedMessage
 	{
+
+		
 		public string text { get; set; }
+
+		
 		public string html { get; set; }
+
+		
 		public string markdown { get; set; }
 	}
 
 	public class Resource
 	{
+		
 		public int id { get; set; }
+		
 		public string buildNumber { get; set; }
+		
 		public string status { get; set; }
+		
 		public string result { get; set; }
+		
 		public DateTime queueTime { get; set; }
+		
 		public DateTime startTime { get; set; }
+		
 		public DateTime finishTime { get; set; }
+		
 		public string url { get; set; }
+		
 		public int buildNumberRevision { get; set; }
+		
 		public string uri { get; set; }
+		
 		public string sourceBranch { get; set; }
+		
 		public string sourceVersion { get; set; }
+		
 		public string priority { get; set; }
+		
 		public string reason { get; set; }
+		
 		public TfsUser requestedFor { get; set; }
+		
 		public TfsUser requestedBy { get; set; }
 	}
 
 	public class TfsUser
 	{
+		
 		public Guid id { get; set; }
+		
 		public string displayName { get; set; }
+		
 		public string uniqueName { get; set; }
+		
 		public string url { get; set; }
+		
 		public string imageUrl { get; set; }
 	}
 }
